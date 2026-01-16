8507dc264e2d0b8bead753b9674fa995d79ad85a
matthiasmiller <matthiasmiller@users.noreply.github.com>
2026-01-16 10:31:10 -0700

    Handle elements inside the A tag.

--- content_script.js
+++ content_script.js
@@ -1,12 +1,11 @@
 (function() {
     document.addEventListener("click", function (e) {
-        if (e.target.tagName === 'A' &&
-            e.target.href.toLowerCase().startsWith('mailto:')) {
-
+        const link = e.target.closest('a');
+        if (link && link.href && link.href.toLowerCase().startsWith('mailto:')) {
             e.preventDefault(); // Prevent a link from following the URL
 
             MailtoAnywhere.getSettings(function(settings) {
-                const url = MailtoAnywhere.parseURL(settings.url, e.target.href);
+                const url = MailtoAnywhere.parseURL(settings.url, link.href);
                 if (settings.openInNewWindow) {
                     window.open(url);
                 } else {
